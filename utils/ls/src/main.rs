1ba2226aab93db74cc8d786cc2887c7d63eb9987
aen-elle <klopkklopk@mail.ru>
2025-10-25 23:11:07 +0300

    remove faulty test

--- utils/ls/src/main.rs
+++ utils/ls/src/main.rs
@@ -12,14 +12,3 @@ pub fn main() {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
